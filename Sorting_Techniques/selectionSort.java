72b1de4a11df476cfa496f2142b71fe237e227bd
kananashish <ashishkananconnect@gmail.com>
2024-10-02 20:01:24 +0530

    new commit

---
+++ Sorting_Techniques/selectionSort.java
@@ -0,0 +1,21 @@
+class selectionSort {
+    public int[] selectionSort(int[] nums) {
+        //size of array
+        int n = nums.length;
+        for(int i = 0; i < n-1; i++){
+            int mini = i;
+            for(int j = i; j < n; j++){
+                if(nums[j] < nums[mini]){
+                    mini = j;
+                }
+            }
+            swap(nums, i, mini);
+        }
+        return nums;
+    }
+    private void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
